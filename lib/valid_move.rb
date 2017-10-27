799fbda76c2903699092d0d2c89de21a6d87a33a
Tom Chan <tomchanthree@gmail.com>
2017-10-27 21:11:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(input)
   real_input = input - 1
-  if real_input < 0 || real input > 8
+  if real_input < 0 || real_input > 8
     return false
   end
 end
