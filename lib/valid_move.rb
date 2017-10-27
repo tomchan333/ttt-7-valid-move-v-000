e316e273cf6bead6bb24e375f56624bde4526c1d
Tom Chan <tomchanthree@gmail.com>
2017-10-27 21:18:28 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,6 @@
 # code your #valid_move? method here
-def valid_move?(input)
-  real_input = input - 1
-  if real_input < 0 || real_input > 8
+def valid_move?(board, input)
+  if input.between?(0, 8) && !(position_taken?(board, input))
     return false
   end
 end
